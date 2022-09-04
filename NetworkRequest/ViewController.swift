69d6d9c20d2b6af2028712f57d1b2aa7da9c7bf9
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-09-04 19:08:44 +0900

    Initial Commit

---
+++ NetworkRequest/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  NetworkRequest
+//
+//  Created by 山本響 on 2022/09/04.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private(set) var button: UIButton!
+    private var dataTask: URLSessionDataTask?
+    var session = URLSession.shared
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+    
+    @IBAction private func butoonTapped() {
+        searchForBook(terms: "out from boneville")
+    }
+    
+    private func searchForBook(terms: String) {
+        guard let encodedTerms = terms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: "https://itunes.apple.com/search?" +
+                            "media=ebook&term=\(encodedTerms)") else { return }
+        
+        let request = URLRequest(url: url)
+        dataTask = session.dataTask(with: request) { [weak self] (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            
+            guard let self = self else { return }
+            
+            let decoded = String(data: data ?? Data(), encoding: .utf8)
+            print("response: \(String(describing: response))")
+            print("data: \(String(describing: decoded))")
+            print("error: \(String(describing: error))")
+            
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.dataTask = nil
+                self.button.isEnabled = true
+            }
+        }
+        button.isEnabled = false
+        dataTask?.resume()
+    }
+
+}
+
